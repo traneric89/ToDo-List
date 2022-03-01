4bf01752a7db540df8f368edf27ba556aa1191be
optiX <55358375+traneric89@users.noreply.github.com>
2022-02-28 21:05:00 -0500

    updated script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class toDo {
     this.complete = complete;
   }
   toDoComplete() {
-    this.complete = !complete;
+    this.complete = !this.complete;
   }
   updatePrio(newPrio) {
     this.prio = newPrio;
@@ -18,3 +18,24 @@ class toDo {
     this.date = date;
   }
 }
+
+const testObj = new toDo("title1", "prio1", true, "detailsHere", "12-24-2022");
+console.log(testObj);
+
+testObj.toDoComplete();
+console.log(testObj);
+
+/*
+list of project array, global???
+need projectObject 
+
+function to add project()
+	event listener for green add button
+	popup for user to enter project name
+	
+
+projectArray = [proj1, proj2, proj3]
+
+function to add a todo to a project()
+	
+*/
